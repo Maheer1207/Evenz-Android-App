f232ce56b4829f08395409e395f08d411f61caaf
maheee1207 <maheer.1207@gmail.com>
2024-03-02 15:43:48 -0700

    changed the navigation implementation

--- andriodApp/app/src/main/java/com/example/evenz/EventCreation.java
+++ andriodApp/app/src/main/java/com/example/evenz/EventCreation.java
@@ -4,35 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.evenz.databinding.EventCreationBinding;
+public class EventCreation extends AppCompatActivity {
 
-public class EventCreation extends Fragment {
-    EventCreationBinding binding;
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_creation);
 
-        binding = EventCreationBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-
+        findViewById(R.id.fab).setOnClickListener(v -> finish());
     }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-
 }
