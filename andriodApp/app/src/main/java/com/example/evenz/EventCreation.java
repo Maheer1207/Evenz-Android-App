05ef523d9b756dec8e1d48474b7e4fbfdbdb7ba8
maheee1207 <maheer.1207@gmail.com>
2024-03-03 08:41:13 -0700

    implemented the UI, need to figure out correct dimension of the screen, and delete/rename unused resources.

--- andriodApp/app/src/main/java/com/example/evenz/EventCreation.java
+++ andriodApp/app/src/main/java/com/example/evenz/EventCreation.java
@@ -1,14 +1,8 @@
 package com.example.evenz;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 public class EventCreation extends AppCompatActivity {
 
@@ -16,8 +10,8 @@ public class EventCreation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event_creation);
+        setContentView(R.layout.create_event);
 
-        findViewById(R.id.fab).setOnClickListener(v -> finish());
+        findViewById(R.id.vector).setOnClickListener(v -> finish());
     }
 }
