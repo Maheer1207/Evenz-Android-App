625cbf44807252e369b0c26ca39c812e56b315bf
Aizaz <matahir@ualberta.ca>
2024-03-08 01:25:09 -0700

    Added menu bar to Attendees UI and implemented back button functionality

--- andriodApp/app/src/main/java/com/example/evenz/AttendeesActivity.java
+++ andriodApp/app/src/main/java/com/example/evenz/AttendeesActivity.java
@@ -1,6 +1,8 @@
 package com.example.evenz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,55 +19,29 @@ public class AttendeesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_attendees_at_event); // Make sure you have a layout file named activity_attendees.xml
+        setContentView(R.layout.view_attendees_at_event);
 
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Initialize mock data
-//        attendeesList = new ArrayList<>();
-//        attendeesList.add(new Attendee("Saske", "1234567890", "780-123-4567", "jhondoe@gmail.com"));
-//        attendeesList.add(new Attendee("Jane Doe", "1234567890", "780-123-4567", "jane@gmail.com"));
-//        attendeesList.add(new Attendee("John Smith", "1234567890", "780-123-4567", "smith@gmail.com"));
-
-        // Mofiy each attendee to all be at the same event
-//        for (Attendee attendee : attendeesList) {
-//            attendee.addEvent("Drake Concert");
-//        }
-
-        // send one of these attendees to the database
         FirebaseAttendeeManager attendeeManager = new FirebaseAttendeeManager();
-        // clear the database
-//        attendeeManager.clearAttendees();
 
-//        attendeeManager.submitAttendee(attendeesList.get(0));
-
-        // Fetch the attendees from the database and print all of them
-//        attendeeManager.getAllAttendeesAsynchronously().addOnCompleteListener(task -> {
-//            if (task.isSuccessful()) {
-//                List<Attendee> attendees = task.getResult();
-//                // Handle the list of attendees
-//            } else {
-//                // Handle the error
-//            }
-//        });
-
-        // Fetch the attendees from the database that are at a specific event and print all of them
         attendeeManager.getEventAttendees("Drake Concert").addOnCompleteListener(task -> {
-              if (task.isSuccessful()) {
+            if (task.isSuccessful()) {
                 attendeesList = task.getResult();
-                // Print all of the attendees names to the console
                 for (Attendee attendee : attendeesList) {
                     System.out.println(attendee.getName());
                 }
                 adapter = new AttendeeAdapter(attendeesList);
                 recyclerView.setAdapter(adapter);
             } else {
-                // Handle the error print a message to console saying that the task was not successful
                 System.out.println("Task was not successful");
             }
         });
+    }
 
+    public void onHeaderIconClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
-
