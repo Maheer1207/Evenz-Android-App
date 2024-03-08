10ffaf8e65325b8dcfa4f493962216d44aefb087
OFazal <ofazal@ualberta.ca>
2024-03-08 12:55:52 -0700

    started javadocs comments

--- andriodApp/app/src/main/java/com/example/evenz/Admin.java
+++ andriodApp/app/src/main/java/com/example/evenz/Admin.java
@@ -1,5 +1,18 @@
 package com.example.evenz;
-
+/**
+ * The {@code Admin} class represents an administrative user, extending the {@code User} class.
+ * It includes additional functionalities and attributes specific to administrative roles.
+ *
+ * <p>Instances of this class can be created with the provided constructor, which initializes
+ * the common user details using the {@code User} constructor.
+ *
+ * <p>This class serves as a foundation for implementing administrative functionalities within
+ * the application.
+ *
+ * @author maheeee1207
+ * @version 1.0
+ * @see User
+ */
 public class Admin extends User
 {
 
