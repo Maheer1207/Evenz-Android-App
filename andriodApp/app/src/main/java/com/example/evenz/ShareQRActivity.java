075db5fdcdd2dfdca9be7f287c316b85562fe908
maheee1207 <maheer.1207@gmail.com>
2024-03-27 03:19:56 -0600

    implemented share generated QR

--- andriodApp/app/src/main/java/com/example/evenz/ShareQRActivity.java
+++ andriodApp/app/src/main/java/com/example/evenz/ShareQRActivity.java
@@ -2,15 +2,21 @@ package com.example.evenz;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -21,6 +27,7 @@ public class ShareQRActivity extends AppCompatActivity {
         setContentView(R.layout.container_qr_code_generated_organizer);
 
         ImageView qr = findViewById(R.id.qrImage);
+        LinearLayout share_QR = findViewById(R.id.share_generated_qr);
 
         Uri bitmapUri = Uri.parse(getIntent().getStringExtra("BitmapImage"));
         String eventID = getIntent().getStringExtra("eventID");
@@ -28,9 +35,16 @@ public class ShareQRActivity extends AppCompatActivity {
         try {
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), bitmapUri);
             qr.setImageBitmap(bitmap);
+            share_QR.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareImageandText(bitmap);
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
         ImageView back = findViewById(R.id.back_qr_share);
         back.setOnClickListener(new View.OnClickListener() {
@@ -44,5 +58,43 @@ public class ShareQRActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void shareImageandText(Bitmap bitmap) {
+        Uri uri = getImageToShare(bitmap);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        // putting uri of image to be shared
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // adding text to share
+        intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image");
+
+        // Add subject Here
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+
+        // setting type to image
+        intent.setType("image/png");
+
+        // calling startactivity() to share
+        startActivity(Intent.createChooser(intent, "Share Via"));
+    }
+
+    // Retrieving the url to share
+    private Uri getImageToShare(Bitmap bitmap) {
+        File imageFolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imageFolder.mkdirs();
+            File file = new File(imageFolder, "shared_image.png");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.anni.shareimage.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
     }
 }
